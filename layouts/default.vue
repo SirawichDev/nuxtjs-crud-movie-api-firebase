177d2d0596ba08b9f0170e7a56df83c77d6bca17
SirawichDev <sirawit0676@gmail.com>
2019-03-24 07:28:31 +0700

    .

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,17 @@
   <div>
 <nav-bar/>
     <nuxt/>
+    <footer/>
   </div>
 </template>
 
 <script>
 import NavBar from './navBar';
+import Footer from './footer'
 export default {
   components:{
-    NavBar
+    NavBar,
+    Footer
   }
 }
 </script>
