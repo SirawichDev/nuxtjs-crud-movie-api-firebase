f120665ef4f6bc4d6a99d5baeebe9c4c161f3230
SirawichDev <sirawit0676@gmail.com>
2019-03-25 02:43:30 +0700

    footer component use

---
+++ components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>MovieDB API</strong> by <a href="https://github.com/SirawichDev">Sirawich Voungchuy</a>.
+    </p>
+  </div>
+</footer>
+</template>
+
+<style scoped>
+  footer{
+    position: relative;
+    bottom: 0;
+  }
+</style>
