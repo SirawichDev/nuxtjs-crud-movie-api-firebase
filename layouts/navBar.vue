9822990ec325f48eddbf312840af0f9c5bdca41c
SirawichDev <sirawit0676@gmail.com>
2019-03-24 07:28:45 +0700

    .

--- layouts/navBar.vue
+++ layouts/navBar.vue
@@ -22,46 +22,27 @@
 <nuxt-link to="/inspire">
 
       <a class="navbar-item">
-        Documentation
+        Create
       </a>
 </nuxt-link>
 
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
 
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
     </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
-      </div>
-    </div>
+
   </div>
 </nav>
 
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style >
+  nuxt-link{
+    margin-top: 10px;
+  }
+</style>
