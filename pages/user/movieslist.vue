5ebd52d45d7f62eba3f24167fffe6d838d4c2fc6
SirawichDev <sirawit0676@gmail.com>
2019-03-26 14:38:43 +0700

    mapGetter movies

---
+++ pages/user/movieslist.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>your Movies</h1>
+    <movie-list :uMovie="uMovies"/>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+import { mapGetters } from "vuex";
+import MovieList from '@/components/user/UserTable'
+export default {
+  components:{
+    MovieList
+  },
+  data(){
+    return{
+
+    }
+  },
+computed:{
+  ...mapGetters(['uMovies'])
+},
+
+}
+</script>
