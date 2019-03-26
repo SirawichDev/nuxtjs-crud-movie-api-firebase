57ef98a567ec7e1a8432f7c7d76bba1a98e1b24a
SirawichDev <sirawit0676@gmail.com>
2019-03-26 20:20:28 +0700

    make title component

---
+++ components/Title/title.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div class="tile is-ancestor">
+  <div class="tile is-vertical is-20">
+    <div class="tile">
+
+    </div>
+    <div class="tile is-parent">
+      <article class="tile is-child notification is-danger">
+        <p class="title">{{title}}</p>
+        <p class="subtitle">{{sub}}</p>
+        <div class="content">
+        </div>
+      </article>
+    </div>
+  </div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  props:['title','sub']
+}
+</script>
+
+<style scoped>
+
+</style>
