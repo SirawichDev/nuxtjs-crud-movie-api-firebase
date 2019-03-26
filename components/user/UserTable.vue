b8b89b39053da35ba08666a0b849db287f62c236
SirawichDev <sirawit0676@gmail.com>
2019-03-26 14:38:50 +0700

    .

---
+++ components/user/UserTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-table
+    :data="data"
+    :columns="columns"
+  ></b-table>
+</template>
+
+<script>
+export default {
+  props: ['uMovie'],
+  data() {
+    return {
+      data: this.uMovie,
+      columns: [
+        {
+          field: 'id',
+          label: 'ID',
+          width: '50',
+          numeric: true
+        },
+        {
+          field: 'mName',
+          label: 'Movies Name'
+        },
+        {
+          field: 'type',
+          label: 'Movie Type'
+        },
+        {
+          field: 'Overview',
+          label: 'Overview',
+          centered: true
+        }
+      ]
+    }
+  }
+}
+</script>
