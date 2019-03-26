b13e3196113458a9a2ee329c9d629153487b6d28
SirawichDev <sirawit0676@gmail.com>
2019-03-26 20:17:34 +0700

    my movies

--- components/user/UserTable.vue
+++ components/user/UserTable.vue
@@ -1,37 +1,88 @@
 <template>
-  <b-table
-    :data="data"
-    :columns="columns"
-  ></b-table>
+  <section>
+    <b-table
+      :data="data"
+      :total="data.length"
+      paginated
+      @page-change="pageChange"
+      backend-pagination
+      :default-sort-direction="defalutSort"
+      :default-sort="[sortField,sortSelect]"
+      backend-sorting
+      :per-page="parPage"
+      @sort="sorTer"
+    >
+      <template slot-scope="props">
+        <b-table-column
+          width="200"
+          field="original_title"
+          label="Movie Title"
+          sortable
+        >
+          <p class="title">{{props.row.mName}}</p>
+        </b-table-column>
+        <b-table-column
+          feild="type"
+          label="type"
+          numeric
+          sortable
+          centered
+        >
+          <span
+            class="tag"
+            :class="typer(props.row.type)"
+          >
+            {{props.row.type}}
+          </span>
+        </b-table-column>
+        <b-table-column label="Overview">{{props.row.Overview | cutText(80)}}</b-table-column>
+         <b-table-column >
+           <nuxt-link :to="`/user/editM/${props.row.id}`">
+           <a class="button is-primary is-outlined">Outlined</a>
+           </nuxt-link>
+          </b-table-column>
+      </template>
+    </b-table>
+  </section>
 </template>
 
 <script>
 export default {
-  props: ['uMovie'],
+  props: ['uMovie', 'loaded'],
   data() {
     return {
       data: this.uMovie,
-      columns: [
-        {
-          field: 'id',
-          label: 'ID',
-          width: '50',
-          numeric: true
-        },
-        {
-          field: 'mName',
-          label: 'Movies Name'
-        },
-        {
-          field: 'type',
-          label: 'Movie Type'
-        },
-        {
-          field: 'Overview',
-          label: 'Overview',
-          centered: true
-        }
-      ]
+      totalPage: 0,
+      loading: false,
+      sortField: 'vote_count',
+      defalutSort: 'desc',
+      sortSelect: 'desc',
+      page: 1,
+      cname: [],
+      parPage: 20
+    }
+  },
+
+  methods: {
+    typer(tmovie) {
+      if (tmovie === 'Action') {
+        return 'is-danger'
+      } else if (tmovie === 'Science Fiction') {
+        return 'is-info'
+      } else if (tmovie === 'Thriller') {
+        return 'is-warning'
+      } else if (tmovie === 'Adventure') {
+        return 'is-success'
+      } else if (tmovie === 'Drama') {
+        return 'is-dark'
+      } else if (tmovie === 'Crime') {
+        return 'is-purple'
+      }
+    }
+  },
+  filters: {
+    cutText(value, length) {
+      return value.length > length ? value.substr(0, length) + '...' : value
     }
   }
 }
