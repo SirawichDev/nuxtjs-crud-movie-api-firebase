2f7171786701cb52a71ee8ef0c9670ef7e93abed
SirawichDev <sirawit0676@gmail.com>
2019-03-26 13:50:27 +0700

    add user path

---
+++ pages/user/createM/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="section">
+    <create-form @sumiter="onSubmit"/>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+import CreateForm from '~/components/Form/Create'
+export default {
+  components:{
+    CreateForm
+  },
+  methods:{
+
+    onSubmit(createData){
+      console.log('data', createData);
+      axios.post('https://nuxty-fbf26.firebaseio.com/movies.json',createData).then(res => console.log(res)).catch(err => console.log(err))
+  }
+  }
+}
+</script>
