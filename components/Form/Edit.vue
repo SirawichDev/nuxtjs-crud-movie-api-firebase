ce8bb72ec5c8769bd86fc5599f11daabb0e49a30
SirawichDev <sirawit0676@gmail.com>
2019-03-27 11:45:01 +0700

    edit form finsihed

--- components/Form/Edit.vue
+++ components/Form/Edit.vue
@@ -1,15 +1,14 @@
 <template>
   <section>
+    <h1>{{movie.id}}</h1>
     <form @submit.prevent="onSave">
 
       <b-field
         horizontal
         label="Movie Name"
-        :type="check"
-        :message="err"
       >
         <b-input
-          v-model="createData.mName"
+          v-model="movie.mName"
           name="subject"
           expanded
         ></b-input>
@@ -20,7 +19,7 @@
         label="From"
       >
         <b-input
-          v-model="createData.postBy"
+          v-model="movie.postBy"
           name="name"
           placeholder="Name"
           expanded
@@ -32,37 +31,48 @@
         label="Type"
       >
 
-<div class="control">
-  <label class="radio">
-    <input v-model="createData.type" id="Adventure" type="radio" value="Adventure">
-    Adventure
-  </label>
-  <label class="radio">
-    <input v-model="createData.type" id="Action" type="radio" value="Action" checked>
-    Action
-  </label>
-   <label class="radio">
-    <input v-model="createData.type" id="Drama" type="radio" value="Drama" checked>
-    Drama
-  </label>
-   <label class="radio">
-    <input type="radio" id="Science Fiction" v-model="createData.type"  value="Science Fiction" checked>
-    Science Fiction
-  </label>
-</div>
-<vue-stars
-name="demo"
-active-color="#1749ff"
-inactive-color="#00ddae"
-shadow-color="#e9ff00"
-hover-color="#4435dd"
-:max="5"
-:value="4"
-:readonly="false"
-char=""
-inactive-char=""
-:class="fa"
-/>
+        <div class="control">
+          <label class="radio">
+            <input
+              v-model="movie.type"
+              id="Adventure"
+              type="radio"
+              value="Adventure"
+            >
+            Adventure
+          </label>
+          <label class="radio">
+            <input
+              v-model="movie.type"
+              id="Action"
+              type="radio"
+              value="Action"
+              checked
+            >
+            Action
+          </label>
+          <label class="radio">
+            <input
+              v-model="movie.type"
+              id="Drama"
+              type="radio"
+              value="Drama"
+              checked
+            >
+            Drama
+          </label>
+          <label class="radio">
+            <input
+              type="radio"
+              id="Science Fiction"
+              v-model="movie.type"
+              value="Science Fiction"
+              checked
+            >
+            Science Fiction
+          </label>
+        </div>
+
       </b-field>
 
       <b-field
@@ -70,7 +80,7 @@ inactive-char=""
         label="Overview"
       >
         <b-input
-          v-model="createData.Overview"
+          v-model="movie.Overview"
           type="textarea"
         ></b-input>
       </b-field>
@@ -92,24 +102,19 @@ inactive-char=""
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  data() {
-    return {
-      createData: {
-        mName: '',
-        postBy: '',
-        release_data: '',
-        type: '',
-        Overview: '',
-      },
-        err: ''
-    }
-  },
   methods: {
     onSave() {
-      console.log(this.createData);
-      this.$emit('sumiter',this.createData);
+      console.log(this.ss)
+      this.$emit('sumiter', this.movie)
     }
+  },
+  computed: {
+    ...mapGetters(['movie'])
+  },
+  created() {
+    console.log('xx', this.movie)
   }
 }
 </script>
