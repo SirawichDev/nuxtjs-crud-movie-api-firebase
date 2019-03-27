3d62cb6d8489ed72084f0e3cf223c2ba5bebb941
SirawichDev <sirawit0676@gmail.com>
2019-03-27 11:45:29 +0700

    updte type color

--- pages/user/movieslist.vue
+++ pages/user/movieslist.vue
@@ -1,26 +1,37 @@
 <template>
   <div>
-    <titles title="Your Movies" sub="Miewmiew"></titles>
-    <movie-list :loaded="loaded" :uMovie="uMovies"/>
+    <titles
+      title="Your Movies"
+      sub="Miewmiew"
+    ></titles>
+    <movie-list
+      :loaded="loaded"
+      @delete="Delete"
+      :uMovie="uMovies"
+    />
   </div>
 </template>
 <script>
 import axios from 'axios'
-import { mapGetters } from "vuex";
+import { mapGetters } from 'vuex'
 import MovieList from '@/components/user/UserTable'
 import Titles from '@/components/Title/title'
 export default {
-  components:{
+  components: {
     MovieList,
     Titles
   },
-  data(){
-    return{
-
+  data() {
+    return {}
+  },
+  methods: {
+    Delete(id) {
+      console.log(id)
+      this.$store.dispatch('deleteMovie', id)
     }
   },
-computed:{
-  ...mapGetters(['uMovies','isloaded'])
-}
+  computed: {
+    ...mapGetters(['uMovies', 'isloaded'])
+  }
 }
 </script>
