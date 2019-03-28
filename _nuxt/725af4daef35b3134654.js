1158f5067951d5b17e7588d1adbf5222d561f9b5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-28 05:29:48 +0000

    Deploy SirawichDev/nuxtjs-crud-firebase to github.com/SirawichDev/nuxtjs-crud-firebase.git:gh-pages

---
+++ _nuxt/725af4daef35b3134654.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{195:function(n,t,e){"use strict";e.r(t);var l=e(11),component=Object(l.a)({},function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("h1",[this._v("Login Page")]),this._v(" "),t("nuxt-child")],1)},[],!1,null,null,null);t.default=component.exports}}]);
